dc61c3f13e253e4b5dea6e8ede0835d85025af9d
Anthony <anthony.yoskovich@gmail.com>
2021-11-26 19:39:51 -0500

    Using fluidrow instead

--- app.R
+++ app.R
@@ -5,15 +5,21 @@ library(shinipsum)
 ui <- fluidPage(
   theme = shinytheme('slate'),
   titlePanel('MI School Data: An Alternative Approach'),
-  sidebarLayout(
-    sidebarPanel(random_text(nwords = 50)),
-    mainPanel(
-      tabsetPanel(type='tabs',
-        tabPanel("About This", random_text(nwords = 100)),
-        tabPanel("Individual Lookup", random_text(nwords = 100, offset = 1)),
-        tabPanel("Comparison", random_text(nwords = 100, offset = 2)),
-        tabPanel("Analyze", random_text(nwords = 100, offset = 3))
+  tabsetPanel(type='tabs',
+    tabPanel("About This", 
+      fluidRow(
+        column(4, random_text(nwords = 100)),
+        column(4, 'foobar', offset = 2)
       )
+    ),
+    tabPanel("Individual Lookup", 
+      random_text(nwords = 100, offset = 1)
+    ),
+    tabPanel("Comparison", 
+      random_text(nwords = 100, offset = 2)
+    ),
+    tabPanel("Analyze", 
+      random_text(nwords = 100, offset = 3)
     )
   )
 )
