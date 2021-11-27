898cb3ea97724b83da50425acd9a13d06ad6fa86
Anthony <anthony.yoskovich@gmail.com>
2021-11-26 19:51:11 -0500

    Adding random scatter plot

--- app.R
+++ app.R
@@ -9,23 +9,31 @@ ui <- fluidPage(
     tabPanel("About This", 
       fluidRow(
         column(4, random_text(nwords = 100)),
-        column(4, 'foobar', offset = 2)
+        column(4, plotOutput(outputId = 'rPlot'), offset = 2)
       )
     ),
-    tabPanel("Individual Lookup", 
-      random_text(nwords = 100, offset = 1)
+    tabPanel("Individual Lookup",
+      fluidRow(
+        column(4, random_text(nwords = 100, offset = 1))
+      )
     ),
-    tabPanel("Comparison", 
-      random_text(nwords = 100, offset = 2)
+    tabPanel("Comparison",
+      fluidRow(
+        column(4, random_text(nwords = 100, offset = 2))
+      )
     ),
-    tabPanel("Analyze", 
-      random_text(nwords = 100, offset = 3)
+    tabPanel("Analyze",
+      fluidRow(
+        column(4, random_text(nwords = 100, offset = 3))
+      )
     )
   )
 )
 
 server <- function(input, output) {
-
+  output$rPlot <- renderPlot({
+    random_ggplot('point')
+  })
 }
 
 shinyApp(ui=ui, server = server)
