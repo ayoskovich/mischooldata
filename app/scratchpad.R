8f9ff059a9ba92c5870a2f588c28568fe95c51c9
Anthony <anthony.yoskovich@gmail.com>
2022-03-19 11:33:29 -0400

    Formatting main performance table

--- app/scratchpad.R
+++ app/scratchpad.R
@@ -10,30 +10,6 @@ compare_df_cols(
   old, new
 )
 
-# Combine peer list
-old %>% 
-  select(
-    AcademicYear, ISDCode, DistrictCode, BuildingCode, 
-    starts_with('PeerBuilding')
-  ) %>% 
-  mutate(peer_list = paste0(c_across(starts_with('PeerBuilding')), collapse=',')) %>% 
-  select(-starts_with('PeerBuilding'))
-
-# Proficiency, Growth, Graduation, EL, PAP, Subgroup, Attendance, Participation
-old %>% 
-  select(-starts_with('PeerBuilding')) %>% 
-  names()
-
-# Dream fct table:
-tribble(
-  ~id, ~category, ~subcategory, ~value,
-  1, 'proficiency', 'letter grade', 'A',
-  1, 'proficiency', 'math rate', .56,
-  1, 'proficiency', 'ela rate', .72,
-  1, 'growth', 'letter grade', .72,
-  ...
-)
-
 IDS <- c(
   'AcademicYear', 'ISDCode', 'DistrictCode', 'BuildingCode'
 )
@@ -44,12 +20,37 @@ DIMS <- c(
   'IdentificationType', 'IdentificationReason'
 )
 
+## -------------------------------------
+## Fact table
 fctTable <- old %>% 
+  select(-starts_with('PeerBuilding')) %>% 
   select(
     all_of(IDS),
     !all_of(DIMS)
+  ) %>% 
+  mutate_all(as.character) %>% 
+  pivot_longer(
+    cols=!c(AcademicYear, ISDCode, DistrictCode, BuildingCode)
+  ) %>% 
+  mutate(category = case_when(
+    str_starts(name, 'Proficiency') ~ 'Proficiency',
+    str_starts(name, 'Growth') ~ 'Growth',
+    str_starts(name, 'GradRate') ~ 'Graduation',
+    str_starts(name, 'EL') ~ 'Engligh Learner',
+    str_starts(name, 'PeerCompare') ~ 'Peer Compare',
+    str_starts(name, 'OnTrackAttendance') ~ 'Attendance',
+    str_starts(name, 'Participation') ~ 'Participation',
+    str_starts(name, 'SubgroupPerformance') ~ 'Subgroup Performance'
+  )) %>% 
+  rename(
+    subcategory = name
+  ) %>% 
+  select(
+    AcademicYear, ISDCode, DistrictCode, BuildingCode, category, subcategory, value
   )
   
+## -------------------------------------
+## Dimension table
 comb_peers <- function(x){
   # Combine list of peer schools from multiple columns
   list(x %>% as.numeric())
