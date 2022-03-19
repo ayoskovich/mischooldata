2b7c368d23b17413e2416e1e216ac53555e0e341
Anthony <anthony.yoskovich@gmail.com>
2022-03-19 11:20:03 -0400

    Combining peer schools

--- app/scratchpad.R
+++ app/scratchpad.R
@@ -23,3 +23,43 @@ old %>%
 old %>% 
   select(-starts_with('PeerBuilding')) %>% 
   names()
+
+# Dream fct table:
+tribble(
+  ~id, ~category, ~subcategory, ~value,
+  1, 'proficiency', 'letter grade', 'A',
+  1, 'proficiency', 'math rate', .56,
+  1, 'proficiency', 'ela rate', .72,
+  1, 'growth', 'letter grade', .72,
+  ...
+)
+
+IDS <- c(
+  'AcademicYear', 'ISDCode', 'DistrictCode', 'BuildingCode'
+)
+DIMS <- c(
+  'ISDName', 'DistrictName', 'BuildingName',
+  'EntityCategory', 'GradeList', 
+  'IsAECSchool', 'AECMeetsLawAssurance', 'AECSummaryStatus',
+  'IdentificationType', 'IdentificationReason'
+)
+
+fctTable <- old %>% 
+  select(
+    all_of(IDS),
+    !all_of(DIMS)
+  )
+  
+comb_peers <- function(x){
+  # Combine list of peer schools from multiple columns
+  list(x %>% as.numeric())
+}
+
+dimTable <- old %>% 
+  rowwise() %>% 
+  mutate(peer_list = comb_peers(c_across(starts_with('PeerBuilding')))) %>% 
+  select(
+    all_of(IDS), 
+    all_of(DIMS),
+    peer_list
+  )
